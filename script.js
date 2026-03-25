4513a65deb1ccd01ffe0c5d36a4d5b9b78daf7d9
Diogo Teixeira <diogoteixeira4000@gmail.com>
2026-03-25 20:24:52 -0300

    Adicionando java

---
+++ script.js
@@ -0,0 +1,10 @@
+// Simulação para adicionar intes ao pedido
+//Selecione todos os botões que possuem a classe "Categoria"
+const botoesAdicionar = document.querySelectorAll
+('.adicionar');
+
+// Seleciona a lista onde os itens do pedido serão exibidos
+const listPedido = document.getElementById('lista-pedido');
+
+//selecione o elemento que exibirá o valor total do pedido
+const totalElemento = document.getElementById('lista-pedido');
